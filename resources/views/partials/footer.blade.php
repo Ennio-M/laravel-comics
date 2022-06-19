baf7767d68b01094d302a4949d4e646d0d7a6bb6
Ennio-M <marrese.ennio@gmail.com>
2022-06-19 21:35:34 +0200

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,68 @@
+<footer>
+    <section class="footer-top">
+        <div class="container">
+            <section class="lists">
+                <div class="links">
+                    <h4>Dc Comics</h4>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h4>Shop</h4>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <h4>Dc</h4>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="links">
+                    <h4>Sites</h4>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </section>
+            <div class="bg-logo">
+            </div>
+        </div>
+    </section>
+    <section class="footer-bottom">
+        <div class="container">
+            <div class="signup">
+                <button>Sign-up now!</button>
+            </div>
+            <section class="socials">
+                <h4>Follow us</h4>
+                <img src="{{asset('images/footer-facebook.png')}}" alt="Facebook Logo">
+                <img src="{{asset('images/footer-twitter.png')}}" alt="Twitter Logo">
+                <img src="{{asset('images/footer-youtube.png')}}" alt="YouTube Logo">
+                <img src="{{asset('images/footer-pinterest.png')}}" alt="Pinterest Logo">
+                <img src="{{asset('images/footer-periscope.png')}}" alt="Periscope Logo">
+            </section>
+        </div>
+    </section>
+</footer>
